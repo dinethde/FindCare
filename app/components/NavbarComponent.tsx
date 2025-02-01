724b53620a4308c725b381e411d97945b05407d4
Dineth De Silva <dinethran33@gmail.com>
2025-02-01 22:05:45 +0530

    Code refactored and add comments

--- app/components/NavbarComponent.tsx
+++ app/components/NavbarComponent.tsx
@@ -1,18 +1,7 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import photo from "../../public/images/photo.png";
-import searchIcon from "../../public/assets/icons/search-icon.svg";
-import homeIcon from "../../public/assets/icons/home-icon.svg";
-import inboxIcon from "../../public/assets/icons/inbox-icon.svg";
-import caregiverIcon from "../../public/assets/icons/caregiver-icon.svg";
-import clientIcon from "../../public/assets/icons/client-icon.svg";
-import calendarIcon from "../../public/assets/icons/calendar-icon.svg";
-import agreementIcon from "../../public/assets/icons/agreement-icon.svg";
-import notificationIcon from "../../public/assets/icons/notification-icon.svg";
-import mobileAppIcon from "../../public/assets/icons/mobile_app-icon.svg";
-import helpIcon from "../../public/assets/icons/help-icon.svg";
-import starsIcon from "../../public/assets/icons/stars-icon.svg";
-// Importing SVGs
+// Importing Icon componets
 import HomeIcon from "./icons/HomeIcon";
 import SearchIcon from "./icons/SearchIcon";
 import InboxIcon from "./icons/InboxIcon";
@@ -27,8 +16,11 @@ import StarsIcon from "./icons/StarsIcon";
 
 const NavbarComponent: NextPage = () => {
   return (
-    <div className="relative bg-main-2 min-h-screen flex flex-col items-start justify-center  w-auto max-w-[16rem] whitespace-nowrap">
+    // Main wrapper
+    <div className="relative bg-main-2 min-h-screen flex flex-col items-start justify-center  w-auto max-w-[16rem] whitespace-nowrap text-grey ">
+      {/* Agency name and logo */}
       <div className="self-stretch flex  justify-start p1-4 p-[0.5rem] pl-3 gap-2">
+        {/* Agency logo */}
         <Image
           className="relative rounded-md object-cover "
           width={30}
@@ -36,8 +28,9 @@ const NavbarComponent: NextPage = () => {
           alt=""
           src={photo}
         />
+        {/* Agency  Name */}
         <div className="flex flex-row items-center justify-start">
-          <b className="font-regular_text">Find Care</b>
+          <b className="font-regular_text text-black">Find Care</b>
         </div>
       </div>
       <div className="flex-1 flex flex-col items-start justify-center p-[1rem] gap-[1.75rem]">
@@ -67,38 +60,53 @@ const NavbarComponent: NextPage = () => {
             </div>
           </div>
         </div>
+
+        {/* Horizontal Line */}
         <div className="self-stretch relative  h-[0.8px] bg-grey" />
+
+        {/* Navbar pages link wrapper */}
         <div className="flex flex-col items-start justify-start gap-[1.25rem]">
+          {/* Caregiver page link */}
           <div className="self-stretch flex flex-row items-center justify-start gap-[0.75rem]">
             <CaregiverIcon />
             <div className="overflow-hidden flex flex-row items-center justify-center">
               <p className="font-regular_text font-medium">Caregivers</p>
             </div>
           </div>
+
+          {/* Client page link */}
           <div className="self-stretch flex flex-row items-center justify-start gap-[0.75rem]">
             <ClientIcon />
             <div className="overflow-hidden flex flex-row items-center justify-center">
               <p className="font-regular_text font-medium">Clients</p>
             </div>
           </div>
+
+          {/* Schedule page link */}
           <div className="self-stretch flex flex-row items-center justify-start gap-[0.75rem]">
             <CalendarIcon />
             <div className="overflow-hidden flex flex-row items-center justify-center">
               <p className="font-regular_text font-medium">Schedules</p>
             </div>
           </div>
+
+          {/* Agreement page link */}
           <div className="self-stretch flex flex-row items-center justify-start gap-[0.75rem]">
             <AgreementIcon />
             <div className="overflow-hidden flex flex-row items-center justify-center">
               <p className="font-regular_text font-medium">Agreements</p>
             </div>
           </div>
+
+          {/* Notification page link */}
           <div className="self-stretch flex flex-row items-center justify-start gap-[0.75rem]">
             <NotificationIcon />
             <div className="w-[7.313rem] overflow-hidden shrink-0 flex flex-row items-center justify-center">
               <p className="font-regular_text font-medium">Notifications...</p>
             </div>
           </div>
+
+          {/* Mobile App */}
           <div className="flex flex-row items-center justify-start gap-[0.75rem]">
             <MobileAppIcon />
             <div className="overflow-hidden flex flex-row items-center justify-center">
@@ -107,23 +115,30 @@ const NavbarComponent: NextPage = () => {
           </div>
         </div>
       </div>
+
+      {/* Navbar bottom wrapper */}
       <div className="self-stretch flex flex-col items-center justify-center gap-[1rem] text-[0.875rem]">
+        {/* Imporatant Links wrapper */}
         <div className="self-stretch flex flex-col justify-start gap-[0.75rem] text-[0.875rem] px-4">
+          {/* Helping page link */}
           <div className="flex flex-row items-center justify-start gap-[0.75rem]">
             <HelpIcon />
             <div className="font-regular_text">Help</div>
           </div>
 
+          {/* Getting started page link */}
           <div className="flex flex-row items-center justify-start gap-[0.75rem]">
             <StarsIcon />
             <div className="font-regular_text">Getting starting</div>
           </div>
         </div>
 
+        {/* Account details wrapper */}
         <div
           className="w-full flex flex-row items-center justify-start p-[0.75rem] gap-[0.5rem] text-[1rem] font-jura"
           style={{ borderTop: "1px solid grey" }}
         >
+          {/* Account profile picture */}
           <Image
             className="w-[1.875rem] relative rounded-[100px] h-[1.875rem] object-cover "
             width={30}
@@ -131,6 +146,8 @@ const NavbarComponent: NextPage = () => {
             alt=""
             src={photo}
           />
+
+          {/* Account name */}
           <b className="text-black">Dineth De Silva</b>
         </div>
       </div>
