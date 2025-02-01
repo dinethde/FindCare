8ce730e6298230c26e9f65f94a77ce72189ca4d1
Dineth De Silva <dinethran33@gmail.com>
2025-02-01 20:48:41 +0530

    Icons component made for search/home/inbox

--- app/components/NavbarComponent.tsx
+++ app/components/NavbarComponent.tsx
@@ -12,49 +12,41 @@ import notificationIcon from "../../public/assets/icons/notification-icon.svg";
 import mobileAppIcon from "../../public/assets/icons/mobile_app-icon.svg";
 import helpIcon from "../../public/assets/icons/help-icon.svg";
 import starsIcon from "../../public/assets/icons/stars-icon.svg";
+// Importing SVGs
+import HomeIcon from "./icons/HomeIcon";
+import SearchIcon from "./icons/SearchIcon";
+import InboxIcon from "./icons/InboxIcon";
 
 const NavbarComponent: NextPage = () => {
   return (
-    <div className="relative bg-main-2 min-h-screen flex flex-col items-start justify-center">
+    <div className="relative bg-main-2 min-h-screen flex flex-col items-start justify-center  w-auto max-w-[16rem] whitespace-nowrap text-grey ">
       <div className="self-stretch flex flex-col items-start justify-center p-[0.5rem] px-[1rem]">
         <div className="flex flex-row items-center justify-start">
           <b className="font-regular_text">Find Care</b>
         </div>
       </div>
       <div className="flex-1 flex flex-col items-start justify-center p-[1rem] gap-[1.75rem]">
-        <div className="self-stretch flex flex-col items-start justify-start py-[0rem] px-[0.25rem] gap-[0.75rem]">
+        {/* Top items wrapper */}
+        <div className="self-stretch flex flex-col items-start justify-start py-[0rem] px-[0.25rem] gap-[1rem]">
+          {/* Search Page Nav Link */}
           <div className="self-stretch flex flex-row items-center justify-start gap-[0.5rem]">
-            <Image
-              className="w-[1rem] relative h-[1rem]"
-              width={16}
-              height={16}
-              alt=""
-              src={searchIcon}
-            />
+            <SearchIcon />
             <div className="flex flex-row items-center justify-center">
               <p className="font-regular_text font-medium">Search</p>
             </div>
           </div>
+
+          {/* Home Page Nav Link */}
           <div className="self-stretch flex flex-row items-center justify-start gap-[0.5rem] text-brand-main">
-            <Image
-              className="w-[1rem] relative h-[1rem] overflow-hidden shrink-0"
-              width={16}
-              height={16}
-              alt=""
-              src={homeIcon}
-            />
+            <HomeIcon color={"#FA5300"} />
             <div className=" flex flex-row items-center justify-center">
-              <p className="font-regular_text font-medium">Home</p>
+              <p className="font-regular_text font-semibold text-main">Home</p>
             </div>
           </div>
+
+          {/* Inbox Page Nav Link */}
           <div className="self-stretch flex flex-row items-center justify-start gap-[0.5rem]">
-            <Image
-              className="w-[1rem] relative h-[1rem]"
-              width={16}
-              height={16}
-              alt=""
-              src={inboxIcon}
-            />
+            <InboxIcon />
             <div className="flex flex-row items-center justify-center">
               <p className="font-regular_text font-medium">Inbox</p>
             </div>
@@ -157,13 +149,13 @@ const NavbarComponent: NextPage = () => {
           style={{ borderTop: "1px solid grey" }}
         >
           <Image
-            className="w-[1.875rem] relative rounded-[100px] h-[1.875rem] object-cover"
+            className="w-[1.875rem] relative rounded-[100px] h-[1.875rem] object-cover "
             width={30}
             height={30}
             alt=""
             src={photo}
           />
-          <b>Dineth De Silva</b>
+          <b className="text-black">Dineth De Silva</b>
         </div>
       </div>
     </div>
