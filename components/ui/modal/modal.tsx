9cbf2eead82d165259a34538fb8a3a299c7a1ec9
Dineth De Silva <dinethran33@gmail.com>
2025-02-07 03:32:58 +0530

    Fix layout issues in the modal

--- components/ui/modal/modal.tsx
+++ components/ui/modal/modal.tsx
@@ -3,8 +3,11 @@ import Link from 'next/link'
 export function Modal({children}: { children: React.ReactNode }) {
     return (
         <div
-            className="absolute top-0 bottom-0 left-0 right-0 m-auto w-[90%] h-[87%] z-10 max-h-screen bg-white p-2 rounded-regular shadow-[0px_2px_8px_rgba(0,_0,_0,_0.15)] overflow-hidden gap-3">
-            <div>{children}</div>
+            className="
+            absolute top-0 bottom-0 left-0 right-0 m-auto w-[90vw] h-[87vh] z-10 max-h-screen bg-white
+             rounded-regular shadow-[0px_2px_8px_rgba(0,_0,_0,_0.15)] overflow-hidden
+            ">
+            <div className="max-h-full">{children}</div>
         </div>
     )
 }
