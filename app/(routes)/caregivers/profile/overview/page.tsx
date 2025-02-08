8816011b15db6c7e73a169cbf06951d4ea02f884
Dineth De Silva <dinethran33@gmail.com>
2025-02-08 16:55:05 +0530

    Refactor caregiver overview and information card components for improved structure and styling

--- app/(routes)/caregivers/profile/overview/page.tsx
+++ app/(routes)/caregivers/profile/overview/page.tsx
@@ -24,44 +24,44 @@ export default function CaregiverOverview() {
         </TabsTrigger>
       </TabsList>
 
-      <TabsContent value="overview" className="flex flex-col gap-4 w-full">
-        {/* Top Row */}
-        <div className="grid grid-cols-[0.75fr_1.25fr_1fr]  gap-4">
-          <CaregiverImg />
+      <div>
+        <TabsContent
+          value="overview"
+          className="flex flex-col gap-4 w-full m-0"
+        >
+          {/* Top Row */}
+          <div className="grid grid-cols-[0.75fr_1.25fr_1fr]  gap-4">
+            <CaregiverImg />
 
-          {/*/!* Description *!/*/}
-          <CaregiverDescription />
+            {/*/!* Description *!/*/}
+            <CaregiverDescription />
 
-          <div className="">
-            {/*/!* Stats Container *!/*/}
-            <div className="flex gap-4 flex-col">
-              <Card />
-              <Card />
+            <div className="">
+              {/*/!* Stats Container *!/*/}
+              <div className="flex gap-4 flex-col">
+                <Card />
+                <Card />
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* Bottom Row - Metrics */}
-        <Metrics />
-      </TabsContent>
+          {/* Bottom Row - Metrics */}
+          <Metrics />
+        </TabsContent>
 
-      <TabsContent value="general">
-        <InformationCard />
-        <div className="">
-          {/* <CaregiverImg /> */}
-          {/*/!* Description *!/*/}
-          {/* <CaregiverDescription /> */}
-        </div>
-      </TabsContent>
+        <TabsContent value="general" className="m-0">
+          <InformationCard />
+        </TabsContent>
 
-      <TabsContent value="security">
-        <div className="bg-white rounded-lg p-6">
-          <h3 className="text-2xl font-semibold mb-4">Security Settings</h3>
-          <p className="text-muted-foreground">
-            Security settings and information will be displayed here.
-          </p>
-        </div>
-      </TabsContent>
+        <TabsContent value="security">
+          <div className="bg-white rounded-lg p-6">
+            <h3 className="text-2xl font-semibold mb-4">Security Settings</h3>
+            <p className="text-muted-foreground">
+              Security settings and information will be displayed here.
+            </p>
+          </div>
+        </TabsContent>
+      </div>
     </Tabs>
   );
 }
