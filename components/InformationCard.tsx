0e7484a62d673fbb6fa5866fc1618ecff1e80b86
Dineth De Silva <dinethran33@gmail.com>
2025-02-08 16:55:05 +0530

    Refactor caregiver overview and information card components for improved structure and styling

--- components/InformationCard.tsx
+++ components/InformationCard.tsx
@@ -15,6 +15,7 @@ import personalDetailsIcons from "@/public/assets/icons/personal-details-icon.sv
 export default function InformationCard() {
   return (
     <div className="container flex gap-4 flex-col">
+      {/* 1st row */}
       <div className="grid grid-cols-[0.75fr_1.25fr_1fr] gap-4">
         {/* Profile Image */}
         <CaregiverImg />
@@ -40,6 +41,7 @@ export default function InformationCard() {
         </SectionCard>
       </div>
 
+      {/* Second Row */}
       <div className="grid grid-cols-3 gap-4">
         {/* Address Section */}
         <SectionCard
