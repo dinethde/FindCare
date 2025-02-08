516c2655e2ea02374c02c4ade51b5d24942d877b
Dineth De Silva <dinethran33@gmail.com>
2025-02-08 12:52:15 +0530

    Refactor caregiver pages for improved readability and consistency

--- app/(routes)/caregivers/profile/clients/page.tsx
+++ app/(routes)/caregivers/profile/clients/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function CgClientsModal() {
-    return (
-        <div>CgClientsModal</div>
-    )
+  return <div>CgClientsModal</div>;
 }
