ada1ff769328ee897f834436a19705cbccf0c76e
rovindie <rovindie.20220811@iit.ac.lk>
2025-03-02 21:24:19 +0530

    Household Screens - Homepage and marketplace - titles and descriptions updates

--- mobile/lib/componets_approved/market_hosted_b_o_x/market_hosted_b_o_x_widget.dart
+++ mobile/lib/componets_approved/market_hosted_b_o_x/market_hosted_b_o_x_widget.dart
@@ -56,55 +56,59 @@ class _MarketHostedBOXWidgetState extends State<MarketHostedBOXWidget> {
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(6.0),
         ),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Icon(
-              FFIcons.khotTub,
-              color: FlutterFlowTheme.of(context).primaryText,
-              size: 26.0,
-            ),
-            Flexible(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    valueOrDefault<String>(
-                      widget.title,
-                      'Hosted By Find Care',
-                    ),
-                    style: FlutterFlowTheme.of(context).headlineSmall.override(
-                          fontFamily: 'Roboto',
-                          fontSize: 14.0,
-                          letterSpacing: 0.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                  ),
-                  Flexible(
-                    child: AutoSizeText(
-                      valueOrDefault<String>(
-                        widget.description,
-                        'Lorem ipsum dolor sit amet consectetur. Orci purus amet tortor nisi massa nisl sit',
-                      ).maybeHandleOverflow(
-                        maxChars: 50,
-                      ),
-                      minFontSize: 14.0,
-                      style: FlutterFlowTheme.of(context).titleSmall.override(
-                            fontFamily: 'Inter',
-                            color: Color(0xFFABABAB),
-                            fontSize: 14.0,
-                            letterSpacing: 0.0,
-                          ),
-                    ),
-                  ),
-                ].divide(SizedBox(height: 8.0)),
+        child: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0.0, 1.0, 0.0, 0.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(
+                FFIcons.khotTub,
+                color: FlutterFlowTheme.of(context).primaryText,
+                size: 26.0,
               ),
-            ),
-          ].divide(SizedBox(width: 16.0)),
+              Flexible(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      valueOrDefault<String>(
+                        widget.title,
+                        'Hosted By Find Care',
+                      ),
+                      style:
+                          FlutterFlowTheme.of(context).headlineSmall.override(
+                                fontFamily: 'Roboto',
+                                fontSize: 14.0,
+                                letterSpacing: 0.0,
+                                fontWeight: FontWeight.w500,
+                              ),
+                    ),
+                    Flexible(
+                      child: AutoSizeText(
+                        valueOrDefault<String>(
+                          widget.description,
+                          'Lorem ipsum dolor sit amet consectetur. Orci purus amet tortor nisi massa nisl sit',
+                        ).maybeHandleOverflow(
+                          maxChars: 50,
+                        ),
+                        minFontSize: 14.0,
+                        style: FlutterFlowTheme.of(context).titleSmall.override(
+                              fontFamily: 'Inter',
+                              color: Color(0xFFABABAB),
+                              fontSize: 14.0,
+                              letterSpacing: 0.0,
+                            ),
+                      ),
+                    ),
+                  ].divide(SizedBox(height: 8.0)),
+                ),
+              ),
+            ].divide(SizedBox(width: 16.0)),
+          ),
         ),
       ),
     );
