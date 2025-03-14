9557a8a6b96b9ab7400e8abe15056457cd925426
Dineth De Silva <dinethran33@gmail.com>
2025-03-14 08:18:27 +0530

    Update HouseholdController to return user-specific message and add deployment workflow for household service

--- backend/household-service/src/main/java/com/findcare/controller/HouseholdController.java
+++ backend/household-service/src/main/java/com/findcare/controller/HouseholdController.java
@@ -12,7 +12,8 @@ public class HouseholdController {
 
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String getHousehold(@ModelAttribute User user) {
+        String message = "Household for user " + user.getEmail() + " is found";
         log.info(user.toString());
-        return "message received";
+        return message;
     }
 }
