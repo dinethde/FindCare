1f21f58670cb3dea5fa4d71d72be2bf11ff65574
Dineth De Silva <dinethran33@gmail.com>
2025-04-02 09:09:37 +0530

    fixing 404 error #1

--- saas/app/tenant/page.tsx
+++ saas/app/tenant/page.tsx
@@ -111,6 +111,7 @@ export default function TenantPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <LoadingSpinner className="w-6 h-6" />
+      <h1 className="text-2xl font-bold mb-4">Tenant</h1>
     </div>
   );
 }
