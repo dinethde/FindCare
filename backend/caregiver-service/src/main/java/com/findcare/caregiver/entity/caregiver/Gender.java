c122a94fdafbf7c008e074673e61bafb4353ae40
rovindie <rovindie.20220811@iit.ac.lk>
2025-04-06 12:31:43 +0530

    initialize commit for gender class

--- backend/caregiver-service/src/main/java/com/findcare/caregiver/entity/caregiver/Gender.java
+++ backend/caregiver-service/src/main/java/com/findcare/caregiver/entity/caregiver/Gender.java
@@ -1,5 +1,10 @@
 package com.findcare.caregiver.entity.caregiver;
 
+/**
+ * Enum representing the gender of a caregiver.
+ * This is used to standardize and restrict the values for gender
+ * to predefined options: male, female, or other.
+ */
 public enum Gender {
     male,
     female,
