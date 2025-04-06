21ebf0b117d732e018755e09b78419a357d4fcbc
rovindie <rovindie.20220811@iit.ac.lk>
2025-04-06 12:18:16 +0530

    initialize commit for household repository class

--- backend/household-service/src/main/java/com/findcare/repository/HouseholdRepository.java
+++ backend/household-service/src/main/java/com/findcare/repository/HouseholdRepository.java
@@ -5,6 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+/**
+ * Repository interface for managing `HouseholdEntity` persistence.
+ * This interface provides methods for performing CRUD operations and custom queries
+ * on the "household" table in the database.
+ * 
+ * It extends the `JpaRepository` interface, which provides built-in methods for
+ * common database operations such as saving, deleting, and finding entities.
+ */
 @Repository
 public interface HouseholdRepository extends JpaRepository<HouseholdEntity, Long> {
     Optional<HouseholdEntity> findByAuth0Identifier(String auth0Identifier);
