6828f32d26e3e7b5565922b0511ecf4e632e238c
rovindie <rovindie.20220811@iit.ac.lk>
2025-04-06 12:14:28 +0530

    initialize commit for household dto class

--- backend/household-service/src/main/java/com/findcare/dto/HouseholdDto.java
+++ backend/household-service/src/main/java/com/findcare/dto/HouseholdDto.java
@@ -2,6 +2,11 @@ package com.findcare.dto;
 
 import lombok.Data;
 
+/**
+ * Data Transfer Object (DTO) for representing household information.
+ * This class is used to transfer household-related data between different layers of the application.
+ * It includes fields such as household ID, user identifiers, and contact details.
+ */
 @Data
 public class HouseholdDto {
     private Long householdId;
