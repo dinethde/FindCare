be6b21dd5a9039e9c3b04ed9ee095bd599450079
rovindie <rovindie.20220811@iit.ac.lk>
2025-04-06 12:39:04 +0530

    initialize commit for Tier enum entity class

--- backend/agency-service/src/main/java/com/findcare/agency/entity/TierEnum.java
+++ backend/agency-service/src/main/java/com/findcare/agency/entity/TierEnum.java
@@ -1,5 +1,10 @@
 package com.findcare.agency.entity;
 
+/**
+ * Enum representing the subscription tiers available for an agency account.
+ * This is used to standardize and restrict the values for subscription tiers
+ * to predefined options: basic, standard, or premium.
+ */
 public enum TierEnum {
     basic,
     standard,
